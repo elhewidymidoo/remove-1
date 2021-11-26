125a060a1f86d21e89be3324b9b97d9a3467ecc7
elhewidymidoo <95102551+elhewidymidoo@users.noreply.github.com>
2021-11-26 23:54:09 +0200

    Add files via upload

---
+++ Program (1).cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace HewidieConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> HewidieLinkedList = new LinkedList<string>();
+
+            var HewidieNode = HewidieLinkedList.AddFirst("Man");
+            HewidieLinkedList.AddAfter(HewidieNode, "Day");
+            HewidieLinkedList.AddLast("Bad");
+            HewidieLinkedList.AddBefore(HewidieNode, "Nice");
+            HewidieLinkedList.AddLast("Null");
+
+            Console.WriteLine("The linked List Before Deletion:  ");
+
+            foreach (var item in HewidieLinkedList)
+            {
+                Console.Write(item + " ");
+            }
+    
+            HewidieLinkedList.RemoveFirst();
+            HewidieLinkedList.RemoveLast();
+            HewidieLinkedList.Remove("Nice");
+            
+            Console.WriteLine();
+            Console.WriteLine("The linked List After Deletion: ");
+
+            foreach (var item in HewidieLinkedList)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program (2).cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace HewidieConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> HewidieLinkedList = new LinkedList<string>();
+
+            var HewidieNode = HewidieLinkedList.AddFirst("Man");
+            HewidieLinkedList.AddAfter(HewidieNode, "Day");
+            HewidieLinkedList.AddLast("Bad");
+            HewidieLinkedList.AddBefore(HewidieNode, "Nice");
+            HewidieLinkedList.AddLast("Null");
+
+            Console.WriteLine("The linked List Before Reversing:  ");
+
+            foreach (var item in HewidieLinkedList)
+            {
+                Console.Write(item + " ");
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("The linked List After Reversing: ");
+
+            foreach (var item in HewidieLinkedList.Reverse())
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
